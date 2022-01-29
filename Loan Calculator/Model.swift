90831fb8bd307c88637800e95cdb36bc8b8e4e87
Ekaterina <kogdato5@gmail.com>
2022-01-29 17:14:41 +0300

    context in CoreData

--- Loan Calculator/Model.swift
+++ Loan Calculator/Model.swift
@@ -3,7 +3,7 @@ import Foundation
 import CoreData
 import UIKit
 
-var user:[Feature]? = []
+var user:[Feature]? 
 let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 
